318c1595505a15a1f5c35fefc68bf09595557f7c
Vu <nguyentuanvu9x@gmail.com>
2023-06-29 10:08:08 +0300

    Added Module: Randomizer

---
+++ toystore_project/src/Randomizer/Weighted_Randomizer.java
@@ -0,0 +1,38 @@
+package Randomizer;
+
+import Toys.Toy;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+
+public class Weighted_Randomizer<T extends Toy>{
+    private class Entry {
+        double accumulatedWeight;
+        T object;
+    }
+
+    private List<Entry> entries = new ArrayList<>();
+    private double accumulatedWeight;
+    private Random rand = new Random();
+
+    public void addEntry(T object) {
+        accumulatedWeight += object.getFrequency();
+        Entry e = new Entry();
+        e.object = object;
+        e.accumulatedWeight = accumulatedWeight;
+        entries.add(e);
+    }
+
+    public T getRandom() {
+        double r = rand.nextDouble() * accumulatedWeight;
+
+        for (Entry entry: entries) {
+            if (entry.accumulatedWeight >= r) {
+                return entry.object;
+            }
+        }
+        return null; //should only happen when there are no entries
+    }
+}
